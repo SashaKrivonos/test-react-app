e3e7357bb8c6ccde0bdb46032e7bcfec8e65e84f
SashaKrivonos <a.krivonos@itransition.com>
2021-05-25 14:15:50 +0300

    feat: informa-bar

---
+++ src/Pages/StudyProject.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InformaBar from '../Components/InformaBar/index.js'
+
+export default function StudyProjects() {
+
+    return(
+        <div>
+            <InformaBar />
+        </div>
+    )
+
+}
